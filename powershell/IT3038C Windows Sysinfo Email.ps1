28dd3db50ce160c2308a3d92b3de0816935db5a5
isomjr <isomjr@mail.uc.edu>
2022-10-17 13:34:54 -0400

    Lab 7

--- powershell/IT3038C Windows Sysinfo Email.ps1
+++ powershell/IT3038C Windows Sysinfo Email.ps1
@@ -1,5 +1,4 @@
 ﻿$IP = (Get-NetIPAddress).IPv4Address | Select-String "192*"
 $DATE = Get-Date
 $BODY = "This machine's IP is " + $IP + ". User is " + $env:USERNAME + ". Hostname is " + $env:USERDOMAIN + ". Powershell Version " + $Host.Version.Major + ". Today's date is " + $DATE.DateTime + "."
-
-Send-MailMessage -To "reedws@ucmail.uc.edu" -From "isomjr@mail.uc.edu" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+Write-Host $BODY
